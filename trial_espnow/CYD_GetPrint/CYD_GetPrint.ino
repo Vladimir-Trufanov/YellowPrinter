e0eda78ddd00d0ebe4f44791f71fd04f155f706a
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2026-06-30 06:51:25 +0300

    Подчищен CYD_GetPrint

--- trial_espnow/CYD_GetPrint/CYD_GetPrint.ino
+++ trial_espnow/CYD_GetPrint/CYD_GetPrint.ino
@@ -1,28 +1,9 @@
 /*
- CYD_GetPrint.ino
+  CYD_GetPrint.ino
 
- Принять сообщение по ESP-NOW и вывести его на экран фонтом по умолчанию
-
+  Принять сообщение по ESP-NOW и вывести его на экран фонтом по умолчанию
   
-    Последовательный пример  - одноадресная передача данных
-    Lucas Saavedra Vaz - 2024
-    
-    Передача данных между двумя ESP32 с использованием протокола ESP-NOW в конфигурации "один к одному" (unicast).
-    Обратите внимание, что для разных интерфейсов используются разные MAC-адреса.
-    Устройства могут работать в разных режимах (точка доступа или станция), но при этом поддерживать связь с помощью ESP-NOW.
-    Единственное требование заключается в том, чтобы устройства подключались к одному и тому же каналу Wi-Fi.
-    Установите MAC-адрес узла в соответствии с устройством, которое будет принимать данные.
-
-    Пример настройки:
-    
-    - Устройство 1: Режим точки доступа с MAC-адресом F6:12:FA:42:B6:E8
-                MAC-адрес узла устанавливается на MAC-адрес станции устройства 2 (F4:12:FA:40:64:4C)
-                
-    - Устройство 2: Режим станции с MAC-адресом F4:12:FA:40:64:4C.
-                MAC-адрес однорангового устройства, установленный на MAC-адрес точки доступа устройства 1 (F6:12:FA:42:B6:E8)
-
-    Устройство, на котором запущен этот sketch, также будет получать и печатать данные с любого устройства, MAC-адрес которого установлен в качестве MAC-адреса однорангового устройства.
-    Чтобы правильно визуализировать передаваемые данные, установите для конца строки в последовательном мониторе значение "Как NL, так и CR".
+  На примере одноадресной передачи данных от Lucas Saavedra Vaz - 2024 ESP_NOW_Serial.ino
 */
 
 #include <Arduino.h>
@@ -50,21 +31,20 @@
  ESP32-2432S028 (CYD) [7] - MAC Address: B8:D6:1A:5C:F9:54
 */
 
-// Set the MAC address of the device that will receive the data
-// For example: F4:12:FA:40:64:4C
-// const MacAddress peer_mac({0xF4, 0x12, 0xFA, 0x40, 0x64, 0x4C});
-const MacAddress peer_mac({0xB8, 0xD6, 0x1A, 0x5C, 0xF9, 0x54}); // AI Thinker ESP32-CAM 6
-
+// Устанавливаем MAC-адрес устройства c которого будут поступать данные
+const MacAddress peer_mac({0xF8, 0xB3, 0xB7, 0xA7, 0xE1, 0x18}); // AI Thinker ESP32-CAM 6
+// Определяем объект для приема данных по ESP_NOW
 ESP_NOW_Serial_Class NowSerial(peer_mac, ESPNOW_WIFI_CHANNEL, ESPNOW_WIFI_IF);
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
-
-  Serial.print("WiFi Mode: ");
-  Serial.println(ESPNOW_WIFI_MODE == WIFI_AP ? "AP" : "Station");
+  // По умолчанию определяем режим станции
+  Serial.print("Режим WiFi: ");
+  Serial.println(ESPNOW_WIFI_MODE == WIFI_AP ? "собственный WiFi" : "как станция");
   WiFi.mode(ESPNOW_WIFI_MODE);
 
-  Serial.print("Channel: ");
+  Serial.print("Канал: ");
   Serial.println(ESPNOW_WIFI_CHANNEL);
   WiFi.setChannel(ESPNOW_WIFI_CHANNEL, WIFI_SECOND_CHAN_NONE);
 
@@ -73,28 +53,32 @@ void setup() {
     delay(100);
   }
 
-  Serial.print("MAC Address: ");
+  Serial.print("MAC адрес CYD: ");
   Serial.println(ESPNOW_WIFI_MODE == WIFI_AP ? WiFi.softAPmacAddress() : WiFi.macAddress());
 
   // Start the ESP-NOW communication
   Serial.println("ESP-NOW communication starting...");
   NowSerial.begin(115200);
   Serial.printf("ESP-NOW version: %d, max data length: %d\n", ESP_NOW.getVersion(), ESP_NOW.getMaxDataLen());
-  Serial.println("You can now send data to the peer device using the Serial Monitor.\n");
+  Serial.println("Теперь можно вводить данные из последовательного порта и передавать по ESP_NOW,");
+  Serial.println("а также выводить полученные данные в последовательный порт и на экран CYD.\n");
 }
 
-void loop() {
+void loop() 
+{
   while (NowSerial.available()) 
   {
     Serial.write(NowSerial.read());
   }
 
-  while (Serial.available() && NowSerial.availableForWrite()) {
-    if (NowSerial.write(Serial.read()) <= 0) {
-      Serial.println("Failed to send data");
+  while (Serial.available() && NowSerial.availableForWrite()) 
+  {
+    if (NowSerial.write(Serial.read()) <= 0) 
+    {
+      Serial.println("Ошибка отправки данных по ESP_NOW");
       break;
     }
   }
-
+  // Делаем паузу на очистку мусора FreeRTOS
   delay(1);
 }
